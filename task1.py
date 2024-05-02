89430b7ddde1db968005890d381eb44e34b3380b
Elvin Safarli <seferlielvin329@gmail.com>
2024-05-03 00:20:43 +0400

    pyproject1

---
+++ task1.py
@@ -0,0 +1,49 @@
+# task 1
+# numList = []
+
+# num1 = int(input("Eded daxil edin: "))
+# num2 = int(input("Eded daxil edin: "))
+# num3 = int(input("Eded daxil edin: "))
+# num4 = int(input("Eded daxil edin: "))
+# num5 = int(input("Eded daxil edin: "))
+
+# numList.extend([num1, num2, num3, num4, num5])
+# numList.sort()
+# print(numList)
+
+# task2
+# txtadd = input("text daxil edin: ")
+# modifiedVersion = []
+# words = txtadd.split()
+# for word in words:
+#     txtSorted = "".join(sorted(word))
+#     modifiedVersion.append(txtSorted)
+#     sentence = ' '.join(modifiedVersion)
+# print(str(sentence))
+
+# task3
+# artim = 0
+# while True:
+#     trial = int(input("Eded daxil edin: "))
+#     artim += 1
+#     if trial == 13:
+#         print(f"tebrikler, {artim} cehtinizde 13 reqemi aldiz")
+#         break
+
+
+
+# task4
+# a = range(1, 101)
+
+# for p in a:
+#     sadeNum = True
+#     for i in range(2, p):
+#         if (p % i) == 0:
+#             sadeNum = False
+#             break
+#     if p == 1:
+#         continue
+#     if sadeNum:
+#         print(p)
+
+
