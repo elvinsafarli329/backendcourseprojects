1af41a664ea663b4bed9b5ad1ef24b51c21c4ae5
Elvin Safarli <seferlielvin329@gmail.com>
2024-05-06 23:35:54 +0400

    task3 python

---
+++ task3.py
@@ -0,0 +1,29 @@
+# class reading():
+#     attribute = "book"
+
+#     def __init__(self, name, type, langauge, page_count):
+#         self.name = name
+#         self.type = type
+#         self.language = langauge
+#         self.page_count = page_count
+
+#     def book_color(self, book_color = True):
+#         if book_color == False:
+#             return "the pages are not yellow"
+#         else:
+#             return "the pages are yellow"
+        
+#     def book_style(self):
+#         return f"{first_book.name} has a hard cover"
+
+# first_book = reading("Inferno", "adventure", "english", 500)
+# second_book = reading("Animal Farm", "classic", "russian", 800)       
+
+
+# print(reading.attribute)
+
+# print(f"{first_book.name} and {second_book.name} are interesting books. {first_book.name} is written in english and has {first_book.page_count} pages.")
+
+# print(f"The pages of {first_book.name} book are {first_book.book_color(True)}")
+
+# print(first_book.book_style())
