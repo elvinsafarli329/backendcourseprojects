d8cbeb6b0f6d10f31eba6412dd9d37b3c9016391
Elvin Safarli <139715835+elvinsafarli329@users.noreply.github.com>
2024-05-15 18:10:03 +0400

    Delete task8packets/Packets/tempCodeRunnerFile.py

--- task8packets/Packets/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-def samitler_al():
-    letter_list = ["a", "e", "i", "o", "u", "ə", "ü", "ı", "ö" ]
-    new_list = []
-    cumle = input("metn daxil edin: ").split()
-    for i in cumle:
-        if i not in letter_list:
-            new_list.append(i)
