422c5d8e7c7e25c37decb139de887b7f3bede5db
Elvin Safarli <139715835+elvinsafarli329@users.noreply.github.com>
2024-05-15 18:07:35 +0400

    Add files via upload

--- task8packets/Packets/samitler.py
+++ task8packets/Packets/samitler.py
@@ -2,11 +2,13 @@ letter_list = ["a", "e", "i", "o", "u", "ə", "ü", "ı", "ö", " "]
 
 def samitler_al():
     cumle = input("metn daxil edin: ").lower()
-    new_list = set()
+    new_list = []
     for i in cumle:
-        if i not in letter_list:
-            new_list.add(i)
-    return list(new_list)
+        if i not in letter_list and i.isalpha() and i not in new_list:
+            new_list.append(i)
+    if new_list:
+        return new_list
+    else:
+        return "Icinde metn olan cavab daxil edin"
 
 print(samitler_al())
-
