2cbbf89949479f15bccc1850b06babc75ee1c3b6
elvinsafarli329 <seferlielvin329@gmail.com>
2024-06-06 09:56:21 +0400

    modified

--- django_project/movies/models.py
+++ django_project/movies/models.py
@@ -29,21 +29,20 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # Add any additional fields here if needed
 
-    # Override the groups field
+
     groups = models.ManyToManyField(
         Group,
-        related_name='customuser_set',  # Unique related name
+        related_name='customuser_set', 
         blank=True,
         help_text='The groups this user belongs to.',
         verbose_name='groups',
     )
 
-    # Override the user_permissions field
+
     user_permissions = models.ManyToManyField(
         Permission,
-        related_name='customuser_permissions_set',  # Unique related name
+        related_name='customuser_permissions_set', 
         blank=True,
         help_text='Specific permissions for this user.',
         verbose_name='user permissions',
